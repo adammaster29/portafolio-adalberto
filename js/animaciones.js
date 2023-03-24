3fb59c88d962f3d6d5fa629d07d6c1c999f0e63e
adammaster29 <104658788+adammaster29@users.noreply.github.com>
2023-03-23 21:15:05 -0500

    actualizacion de modo nocturno

---
+++ js/animaciones.js
@@ -0,0 +1,9 @@
+
+
+   function modonocturno() {
+    document.body.classList.toggle('modo-nocturno');
+    
+    const icono = document.querySelector(".bx-moon");
+    icono.classList.toggle("bx-sun");
+  }
+  
